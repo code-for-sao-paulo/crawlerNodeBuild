7d6996f8e15f98600ee5195f059dac50abd263e8
Hipólito Douglas França Moreira <Hiplito Douglas Frana Moreira>
2016-08-21 19:19:03 -0300

    agora pega as mensagens do feed, comm, e likes

--- crawler.js
+++ crawler.js
@@ -17,9 +17,8 @@ request(uri, function(error, response, body) {
    }
 });*/
 
+/*
 var accessToken;
-var FB = require('fb');
-
 FB.api('oauth/access_token', {
     client_id: 'app_id',
     client_secret: 'app_secret',
@@ -31,24 +30,51 @@ FB.api('oauth/access_token', {
     }
     accessToken = res.access_token;
 });
+*/
+var access_token = 'EAACEdEose0cBALsJS443oKOYaNfSZB1nuwniBnmt29iqBsvN7Rfgo81LiYwGJAmd3hK7avv7XMpH5GZAXvxY28ZAGWxW6XHODWFoL7Lad1CPqpOjgfd1XseSCVq8EdLoZCXc0ZBgyDa2F2BqyjDmKIuIlVWQ7BtZCnJXDPrGBHZBgZDZD';
+var parameters = "id,name,feed{message,comments{comment_count,like_count}}";
+var candidate = 'fernandoHaddad';
 
+query_data_from_candidate(candidate, parameters, access_token);
 
-FB.setAccessToken('EAACEdEose0cBAGfPxKNSZCZAuRqjHYgEtZA7yqSnzm2s0uGEj9ZB1tlW8UJciMYZCdBmsCzq0doBKELK5lQBdMD77JkZCWZCZCcCzc3HGya5vAi6YA2cHZAG9EaKZB2axLN7f2WZAiaQritGPcB3aH0ZCH5ZAioy2qUumqD1IpDVZBHjHP2wZDZD');
+function query_data_from_candidate(user, parameters, access_token) {
+  var FB = require('fb');
+  FB.setAccessToken(access_token);
+  FB.api(
+    '/' + user,
+    'GET',
+    { "fields": parameters },
+    function (response) {
+      response.feed.data.forEach(function (element) {
+        console.log("Mensagem do feed: "+element.message);
+        var total_comments=0,total_likes=0;
+        element.comments.data.forEach(function (element) {
+          total_comments += element.comment_count;
+          total_likes += element.like_count;
+        }, this);
+        console.log("Total de comentários: "+total_comments);
+        console.log("Total de likes: "+total_likes);
+        console.log('-------------------------------------------');
+      }, this);
+      
+      //console.log(response.feed.data[1].comments.data[0]);
+    }
+  );
+}
 
-FB.api(
+/*FB.api(
   '',
   'POST',
   {
     batch:[
       { method:'get',relative_url:'/fernandohaddad' },
-      { method:'get',relative_url:'/fernandohaddad/feed?limit=10{comments}' },
+      { method:'get',relative_url:'/fernandohaddad/feed?{message,comments{comment_cont,like_count}}' },
     ]
   },
   function(response) {
     var res0, res1;
     res0 = JSON.parse(response[0].body);
     res1 = JSON.parse(response[1].body);
-   
-    console.log(res);
+    console.log(res1);
   }
-);
+);*/
