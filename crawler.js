4e3aa5338c5691d88a159f63ef8d7a01f546f28f
Hipólito Douglas França Moreira <Hiplito Douglas Frana Moreira>
2016-09-13 08:06:06 -0300

    Algumas novas tentativas

--- crawler.js
+++ crawler.js
@@ -1,30 +1,49 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
-
-var access_token = 'EAAS0aIMAt4kBAJPWZBn4ZC2N5wl8uYZCAzystVCi8MUDPxnKyUq7iSUMXKi9SqgtdqxvoW7dHmVmyDWDwg0qZCYhONha3D1kLbBI1D2QsAbDQUQxx0F3zjCl5ZByuZB5fNzEoSYwz6z9IGomT1XBHj9123hTEmxZBJZAh5FwsIbMXwZDZD';
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var parameters = "id,name,feed{message,comments{comment_count,like_count}}";
 var candidate = 'fernandoHaddad';
+  var FB = require('fb');
 
-query_data_from_candidate(candidate, parameters, access_token);
+var urlDB = 'mongodb://localhost:32768/test';
+MongoClient.connect(urlDB, function (err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  var candidate_collection = db.collection('candidates');
 
-/*
+  var candidate1 = { name: "Fernando Haddad", facebook_name: "fernandoHaddad" };
+  var candidate2 = { name: "Celso Russomanno", facebook_name: "100003613814366" };
+
+  candidate_collection.insert([candidate1, candidate2], function (err, result) {
+    if(err){
+      console.log(err);
+    } else {
+      console.log('Inserted %d candidates into the "candidates" collection. The candidates inserted with "_id" are:', result.length, result);
+    }
+  })
+
+  db.close();
+});
+
 var accessToken;
 FB.api('oauth/access_token', {
-    client_id: 'app_id',
-    client_secret: 'app_secret',
+    client_id: '1815746298657244',
+    client_secret: '310c0cd744b3a5d4ab533362a272c265',
     grant_type: 'client_credentials'
 }, function (res) {
     if(!res || res.error) {
         console.log(!res ? 'error occurred' : res.error);
         return;
     }
+    console.log(res.access_token);
     accessToken = res.access_token;
 });
-*/
+
+query_data_from_candidate(candidate, parameters, access_token);
 
 function query_data_from_candidate(user, parameters, access_token) {
-  var FB = require('fb');
 
   FB.setAccessToken(access_token);
 
